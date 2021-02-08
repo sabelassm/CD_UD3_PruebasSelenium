fac3858e4789a5021e224439ed33b65519dc9941
Salva <salvavm90@gmail.com>
2021-02-08 21:35:23 +0100

    Cambios

--- src/App.java
+++ src/App.java
@@ -1,94 +1,68 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");     
+        String username = "testejercicio@hotmail.com";
+        String pass = "ejercicio11";
+
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.get("file:///Users/sabelasobrinosanmartin/seleniumTest/pagina_web/index.html"); //Abrimos el fichero de pruebas combobox
+        driver.get("https://www.pccomponentes.com/");
+
+        // ! Imprimir título de la página
+        String titulo = driver.getTitle();
+        System.out.println(titulo);
+        System.out.println("====================================================");
+
+        // ! ===============  LOGIN  =============== ! //
+        WebElement goToLoginButton = driver.findElement(By.className("qa-user-login-button"));
+        goToLoginButton.click();
+
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/button[2]"));
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(pass);
+        loginButton.click();
         Thread.sleep(3000);
 
-        /* BUSQUEDA TEXTO */
-        driver.get("https://www.wikipedia.es"); //Abrimos la p�gina de la wikipedia en el navegador que acabamos de abrir
-        Thread.sleep(2000); //Para ver la ejecuci�n, detenemos el hilo 2 segundos
+        // ! ===============  COOKIE  =============== ! //
+        WebElement cookieButton = driver.findElement(By.className("accept-cookie"));
+        cookieButton.click();
+        Thread.sleep(3000);
         
-        WebElement searchBox = driver.findElement(By.id("searchInput"));
-        WebElement searchButton = driver.findElement(By.id("searchButton"));
-        
-        Thread.sleep(2000);
-        searchBox.sendKeys("Selenium");
-        Thread.sleep(2000);
-        searchButton.click();
-        Thread.sleep(2000);
-        
-        String titulo=driver.getTitle();
-        System.out.println(titulo); //Visualizamos en consola el t�tulo de la p�gina que se abre como resultado de la b�squeda
-        
+        // ! ===============  SELECCIONAR ELEMENTO Y AÑADIR AL CARRITO  =============== ! //
+        WebElement itemButton = driver.findElement(By.xpath("/html/body/div[3]/div/div[4]/div[4]/div[1]/div/div/div[2]/a"));
+        itemButton.click();
+        Thread.sleep(3000);
 
-        /* PÁGINA ALEATORIA */
-        driver.findElement(By.id("n-randompage")).click();//Hacemos click en el enlace de p�gina aleatoria que hay en el men� izquierdo de wikipedia
-       // driver.findElement(By.linkText("Pagina aleatoria")).click();//Hacemos click en el enlace de p�gina aleatoria que hay en el men� izquierdo de wikipedia
-        
-        Thread.sleep(2000);
-        titulo = driver.getTitle();//Obtenemos el t�tulo de la p�gina web aleatoria que nos abri� wikipedia
-        System.out.println(titulo); //Visualizamos en consola el t�tulo de la p�gina que se abri�
+        WebElement addToCartButton = driver.findElement(By.id("add-cart"));
+        addToCartButton.click();
+        Thread.sleep(3000);
 
-            
-        /* COMBO BOX SIMPLE */
-        Select comboboxSimple = new Select(driver.findElement(By.id("combobox1")));
-        comboboxSimple.selectByIndex(2);//0 es el primer elemento
+        // ! ===============  LOGOUT Y LOGIN  =============== ! //
+        WebElement menuButton = driver.findElement(By.id("user-menu-panel"));
+        menuButton.click();
+        Thread.sleep(3000);
+        WebElement logoutButton = driver.findElement(By.xpath("/html/body/header/div[3]/ul/li[2]/ul/li[6]/a"));
+        logoutButton.click();
+        Thread.sleep(3000);
 
-        Thread.sleep(2000);
-        
-        //OJO: no se puede deseleccionar en un combo Simple
-        driver.findElement(By.id("enviaComboboxes")).click();//hacemos click sobre el bot�n enviar
-        Thread.sleep(2000);
-        /* COMBO BOX MULTIPLE */
-         Select comboboxMultiple = new Select(driver.findElement(By.id("combobox2")));
-        comboboxMultiple.selectByIndex(0);//0 es el primer elemento del combo
-        comboboxMultiple.selectByValue("pera");//seleccionamos por el texto del atributo value de las opciones
-        comboboxMultiple.selectByVisibleText("Plátano");//seleccionamos por el texto visible de las opciones
+        // * Hay que volver a establecer las variables de los elementos por cambio de página
+        goToLoginButton = driver.findElement(By.className("qa-user-login-button"));
+        goToLoginButton.click();
+        usernameInput = driver.findElement(By.name("username"));
+        passwordInput = driver.findElement(By.name("password"));
+        loginButton = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/button[2]"));
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(pass);
+        loginButton.click();
+        Thread.sleep(3000);
 
-        Thread.sleep(2000);//esperamos 2 segundos para poder visualizar las acciones ejecutadas
-        
-        comboboxMultiple.deselectByIndex(1); //Deseleccionamos la segunda opci�n del combo m�ltiple que previamente habiamos seleccionado
-        //OJO: no se puede deseleccionar en un combo Simple
-        
-        driver.findElement(By.id("enviaComboboxes")).click();;//hacemos click sobre el bot�n enviar
-        
-
-        /*RADIO*/
-
-        
-        WebElement aguaBoton = driver.findElement(By.cssSelector("[name='bebida'][value='agua']"));
-        aguaBoton.click();
-        Thread.sleep(2000);		
-        
-        //Usamos xpath para localizar el radioButton comida cuyo valor sea dorada
-        WebElement doradaBoton = driver.findElement(By.xpath("//input[@name='comida' and @value='dorada']"));
-        doradaBoton.click();
-        Thread.sleep(2000);						
-        
-        WebElement botonRadioButton = driver.findElement(By.id("enviaRadiobutton"));//hacemos click sobre el bot�n enviar
-        botonRadioButton.click();
-        
-
-        /*CALENDARIO*/
-
-        
-        WebElement calendario = driver.findElement(By.name("fecha"));
-        calendario.sendKeys("15012021");//Introducimos la fecha 15/01/2021
-        calendario.sendKeys(Keys.TAB);//Introduce la tecla Tabulador para poder cambiar a escribir la hora en el campo
-        calendario.sendKeys("1030");//Escribimos la hora 10:30 en el campo
-        
-
-        Thread.sleep(5000);
         driver.quit();
-
     }
 }
--- src/TestCases.java
+++ src/TestCases.java
@@ -1,70 +1,206 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestCases {
-	
-	private WebDriver driver;
-	
+    private WebDriver driver;
+    private String driverRoute = "E:/CICLO/Entornos de desarrollo/Pruebas Salva/SeleniumPCComponentes/TestCompleto/driver/chromedriver.exe";
+    private String siteUrl = "https://www.pccomponentes.com/";
 
-	@Before
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "/Users/sabelasobrinosanmartin/seleniumTest/driver/chromedriver");     
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", driverRoute);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        System.out.println("=================== TESTING SETUP ===================");
+    }
 
-        driver = new ChromeDriver();
+    @Test // ! -----------------------> F2
+    // * La cantidad de elementos en carrito al cerrar e iniciar sesión debe ser 0
+    public void testCarritoSesion() throws InterruptedException {
+        String username = "testejercicio@hotmail.com";
+        String pass = "ejercicio11";
+        driver.get(siteUrl);
 
-	}
-	
-	@Test
-	public void realizarBusquedaWiki() {
-		
-		driver.get("https://www.wikipedia.es"); //Abrimos la p�gina de la wikipedia 						
+        // * ===============  LOGIN  =============== * //
+        WebElement goToLoginButton = driver.findElement(By.className("qa-user-login-button"));
+        goToLoginButton.click();
 
-						
-			WebElement searchBox = driver.findElement(By.id("searchInput"));
-			WebElement searchButton = driver.findElement(By.id("searchButton"));
-			 String tituloSeleniumEsperado = "Selenium";
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/button[2]"));
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(pass);
+        loginButton.click();
+        Thread.sleep(3000);
 
-			searchBox.sendKeys("Selenium");//Escribimos el texto a buscar en la caja de texto
-			
-			searchButton.click();//Ejecutamos la accion de click para que realice la b�squeda
-			
-			WebElement tituloPrimeraPagina = driver.findElement(By.id("firstHeading"));//Recuperamos el elemento que tiene el titulo en la p�gina abierta
-		
-			String titulo=tituloPrimeraPagina.getText();//Obtenemos el titulo de la pagina abierta
+        // * ===============  COOKIE  =============== * //
+        WebElement cookieButton = driver.findElement(By.className("accept-cookie"));
+        cookieButton.click();
+        Thread.sleep(3000);
+        
+        // * ===============  SELECCIONAR ELEMENTO Y AÑADIR AL CARRITO  =============== * //
+        WebElement itemButton = driver.findElement(By.xpath("/html/body/div[3]/div/div[4]/div[4]/div[1]/div/div/div[2]/a"));
+        itemButton.click();
+        Thread.sleep(3000);
 
-			assertEquals(tituloSeleniumEsperado,titulo);//Comprobamos el titulo obtenido con el esperado
-			
-			
-	}//Fin realizarBusquedawiki
-	
-	@Test
-	public void test_WikiRandomAndBack() {
-		/*******
-		 * Vamos abrir una página de la wikipedia, pulsaremos en el enlace Pagina Aleatoria y a continuaci�n,
-		 * pulsaremos sobre Go Back del navegador
-		 *****/
-		
-			 String tituloHomeEsperado="Wikipedia, la enciclopedia libre";
-			driver.get("https://www.wikipedia.es"); //Abrimos la p�gina de la wikipedia 						
-			WebElement randomPage = driver.findElement(By.linkText("Página aleatoria"));
-			randomPage.click();//Ejecutamos la accion de click para que pinche en el enlace
-			
-			//driver.navigate().back();//Ejecutamos el Atrás del navegador para cargar de nuevo la p�gina inicial
-			
-			String titulo=driver.getTitle();//Obtenemos el titulo de la pagina abierta
-			
-			assertEquals(tituloHomeEsperado,titulo);//Comprobamos el titulo obtenido con el esperado
-			
-			
-	}//Fin test_WikiRandomAndBack
-	
-	@After
-	public void shutdown() {
-		driver.quit();
-	}
+        WebElement addToCartButton = driver.findElement(By.id("add-cart"));
+        addToCartButton.click();
+        Thread.sleep(3000);
+
+        // * ===============  LOGOUT Y LOGIN  =============== * //
+        WebElement menuButton = driver.findElement(By.id("user-menu-panel"));
+        menuButton.click();
+        Thread.sleep(3000);
+        WebElement logoutButton = driver.findElement(By.xpath("/html/body/header/div[3]/ul/li[2]/ul/li[6]/a"));
+        logoutButton.click();
+        Thread.sleep(3000);
+
+        // * Hay que volver a establecer las variables de los elementos por cambio de página
+        goToLoginButton = driver.findElement(By.className("qa-user-login-button"));
+        goToLoginButton.click();
+        usernameInput = driver.findElement(By.name("username"));
+        passwordInput = driver.findElement(By.name("password"));
+        loginButton = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/button[2]"));
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(pass);
+        loginButton.click();
+        Thread.sleep(3000);
+
+        // * OBTENER NÚMERO DE ELEMENTOS EN CARRITO ! //
+        WebElement cartLabel = driver.findElement(By.className("js-units"));
+        assertEquals("0", cartLabel.getText());
+    }
+
+    @Test // ! -----------------------> F3
+    // * Comprobación de mail válido
+    public void comprobacionValidedMail() throws InterruptedException {
+        driver.get(siteUrl);
+        driver.manage().window().maximize();
+
+        // * =============== LOGIN =============== * //
+        WebElement goToLoginButton = driver.findElement(By.className("qa-user-login-button"));
+        String createmail = "estemailnoexisteporquelodigoyo892hotmail.com";
+        goToLoginButton.click();
+        Thread.sleep(3000);
+
+        // * =============== Pulsar Boton Crear =============== * //
+        WebElement clickCreateButton = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/a"));
+        clickCreateButton.click();
+        Thread.sleep(3000);
+
+        // * =============== Introducir E-mail =============== * //
+        WebElement createMailInput = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/div[4]/div/input"));
+        createMailInput.sendKeys(createmail);
+        Thread.sleep(3000);
+        
+        // * OBTENER MENSAJE ! //
+        WebElement messageLabel = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/div[4]/span"));
+        assertEquals("¡Ey! Tu e-mail debe contener @ y dominio", messageLabel.getText());
+    }
+
+    @Test // ! -----------------------> F5
+    public void comprobacionNumeroTelefono() throws InterruptedException {
+        String username = "testejercicio@hotmail.com";
+        String pass = "ejercicio11";
+        String badNumber = "98874541A";
+        driver.get(siteUrl);
+
+        // ! ===============  LOGIN  =============== ! //
+        WebElement goToLoginButton = driver.findElement(By.className("qa-user-login-button"));
+        goToLoginButton.click();
+
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/form/button[2]"));
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(pass);
+        loginButton.click();
+        Thread.sleep(3000);
+
+        // ! ===============  COOKIE  =============== ! //
+        WebElement cookieButton = driver.findElement(By.className("accept-cookie"));
+        cookieButton.click();
+        Thread.sleep(3000);
+        
+        // ! ===============  IR A MI CUENTA =============== ! //
+        WebElement menuButton = driver.findElement(By.id("user-menu-panel"));
+        menuButton.click();
+        Thread.sleep(3000);
+        WebElement miCuentaButton = driver.findElement(By.className("qa-user-login-sub-4"));
+        miCuentaButton.click();
+        Thread.sleep(3000);
+        
+        // ! ===============  IR A AÑADIR DIRECCION =============== ! //
+        WebElement anadirDirecButton = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div/div/div[2]/div[9]/button"));
+        anadirDirecButton.click();
+        Thread.sleep(3000);
+        
+        // ! ===============  IR A AÑADIR TELEFONO INCORRECTO =============== ! //
+        WebElement phoneInput = driver.findElement(By.id("userNewShippingAddress_cellPhone"));
+        phoneInput.sendKeys(badNumber);
+        phoneInput.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+
+        // * OBTENER MENSAJE ! //
+        WebElement messageLabel = driver.findElement(By.id("userNewShippingAddress_cellPhone-error"));
+        assertEquals("Por favor, escribe sólo dígitos.", messageLabel.getText());
+        // assertTrue(messageLabel.getText().length() == 0);
+    }
+
+    @Test // ! -----------------------> F7
+    public void testAvisoStock() throws InterruptedException {
+        String mail = "estamailnoexisteporquelodigoyo389@gmail.com";
+        driver.get("https://www.pccomponentes.com/amd-ryzen-threadripper-3960x");
+
+        // * =============== Pulsar Avisame =============== * //
+        WebElement goToAvisameButton = driver.findElement(By.id("notify-me"));
+        goToAvisameButton.click();
+
+        // * =============== <Escribir> Notificacion. =============== * //
+        WebElement goToNotifyInput = driver.findElement(By.id("Notify_email"));
+        goToNotifyInput.sendKeys(mail);
+
+        // * =============== <Pulsar Boton Check =============== * //
+        WebElement condiciones = driver.findElement(By.className("c-indicator"));
+        condiciones.click();
+
+        // * =============== <Pulsar Boton Enviar =============== * //
+        WebElement buttonSend = driver.findElement(
+                By.xpath("/html/body/div[3]/div[2]/div/div[4]/div[1]/div[6]/div[2]/div/form/div[1]/div[3]/button"));
+        buttonSend.click();
+        
+        // * OBTENER MENSAJE ! //
+        WebElement messageLabel = driver.findElement(By.id("alerta-00"));
+        assertEquals("El aviso se ha registrado correctamente", messageLabel.getText());
+        // assertTrue(messageLabel.getText().length() == 0);
+        
+    }
+
+    @Test // ! -----------------------> F8
+    public void testBusquedaErronea() throws InterruptedException {
+        driver.get(siteUrl);
+ String textoPrueba = "@@@ºº!!";
+        WebElement searchInput = driver.findElement(By.id("ais-autocomplete-selectized"));
+        searchInput.sendKeys(textoPrueba);
+        searchInput.sendKeys(Keys.RETURN);
+        Thread.sleep(3000);
+        // * OBTENER MENSAJE ! //
+        WebElement messageLabel = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div[1]/div/div/h1/span[2]"));
+        assertEquals("\"" + textoPrueba + "\"", messageLabel.getText());
+    } 
+
+    @After
+    public void shutdown() {
+        driver.quit();
+    }
+
 }
