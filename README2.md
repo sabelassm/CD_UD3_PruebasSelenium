6f6b6596fc836f5c4a02cbafc123064a03da8bb0
Eri <erifc84@gmail.com>
2021-02-09 12:22:13 +0100

    Añadidas pruebas junit + selenium y README2

---
+++ README2.md
@@ -0,0 +1,102 @@
+# README
+
+## Alumno
+
+Eri Fernández Cruz
+
+## Cambios
+
+Los cambios para la práctica han sido realizado en la clase TestCases.java
+Modificando también chromedirver y la ruta al mismo.
+
+## Pruebas y resultados
+
+### Prueba: CPF2
+
+**Tipo de prueba**: Funcionales.
+
+**Descripción**: El usuario puede ver artículos que cuenten con alguna oferta.
+
+**Prerrequisitos**: Existir algún producto en oferta.  
+
+**Pasos**:
+1. Entrar en la página.
+2. Pulsar botón “Ofertas especiales”.
+
+**Resultado esperado**: Se visualiza una lista de productos con ofertas. 
+
+**Resultado Obtenido**: Error en la prueba, se visualizan productos con ofertas y productos sin ofertas.
+
+### Prueba: CPF4
+
+**Tipo de prueba**: Funcionales
+
+**Descripción**: El usuario no puede  seleccionar una cantidad a comprar negativa. 
+
+**Prerrequisitos**: N/A.
+
+**Pasos**:
+
+1. Entrar en la página.
+2. Entrar en una sección cualquiera.
+3. Seleccionar un producto con stock.
+4. Pulsar el botón “menos” del campo cantidad intentando poner un número negativo.
+
+**Resultado esperado**: No es posible seleccionar una cantidad negativa. 
+
+**Resultado Obtenido**: Resultado esperado.
+
+### Prueba: CPF5
+
+**Tipo de prueba**: Funcionales.
+
+**Descripción**: El usuario no puede  seleccionar una cantidad a comprar negativa. 
+
+**Prerrequisitos**: N/A.
+
+**Pasos**:
+
+1. Entrar en la página.
+2. Entrar en una sección cualquiera.
+3. Seleccionar un producto con stock.
+4. Seleccionar con el cursor del ratón el número de la cantidad del producto y escribir un número negativo con el teclado.
+
+**Resultado esperado**: No es posible seleccionar una cantidad negativa. 
+
+**Resultado Obtenido**: Resultado esperado.
+
+### Prueba: CPF6
+
+**Tipo de prueba**: Funcionales
+
+**Descripción**: Visualizar productos en orden de precio descendente. 
+
+**Prerrequisitos**: N/A.
+
+**Pasos**:
+
+1. Entrar en la página.
+2. Entrar en una sección cualquiera.
+3. En el combo box seleccionar opción “Precio: más caros primero”.
+
+**Resultado esperado**: Lista de productos ordenados desde el producto con el precio más caro hasta el producto con el precio más barato. 
+
+**Resultado Obtenido**: Resultado esperado.
+
+### Prueba: CPF7
+
+**Tipo de prueba**: Funcionales
+
+**Descripción**: Visualizar productos en orden de precio ascendente. 
+
+**Prerrequisitos**: N/A.
+
+**Pasos**:
+
+1. Entrar en la página.
+2. Entrar en una sección cualquiera.
+3. En el combo box seleccionar opción “Precio: más baratos primero”.
+
+**Resultado esperado**: Lista de productos ordenados desde el producto con el precio más barato hasta el producto con el precio más caro. 
+
+**Resultado Obtenido**: Resultado esperado.
--- src/TestCases.java
+++ src/TestCases.java
@@ -1,67 +1,239 @@
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class TestCases {
-	
+
 	private WebDriver driver;
-	
 
 	@Before
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "/Users/sabelasobrinosanmartin/seleniumTest/driver/chromedriver");     
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Dropbox\\Ciclo Dam\\MP0487. Contornos de desenvolvemento\\UD03\\tarea3\\CD_UD3_PruebasSelenium\\driver\\chromedriver.exe");
 
-        driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
 	}
-	
+
 	@Test
-	public void realizarBusquedaWiki() {
-		
-		driver.get("https://www.wikipedia.es"); //Abrimos la p�gina de la wikipedia 						
+	public void comprobarOfertas() {
 
-						
-			WebElement searchBox = driver.findElement(By.id("searchInput"));
-			WebElement searchButton = driver.findElement(By.id("searchButton"));
-			 String tituloSeleniumEsperado = "Selenium";
+		driver.get("https://www.pccomponentes.com"); // Abrimos la p�gina de pccomponentes
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<WebElement> botones = driver.findElements(By.className("enlaces-clave__tarjeta__a--rounded-border"));
+		WebElement ofertas = null;
+		for (WebElement we : botones) {
+			if (we.getAttribute("data-name").equals("/ofertas-especiales - Ofertas Especiales")) {
+				ofertas = we;
+			}
+		}
 
-			searchBox.sendKeys("Selenium");//Escribimos el texto a buscar en la caja de texto
-			
-			searchButton.click();//Ejecutamos la accion de click para que realice la b�squeda
-			
-			WebElement tituloPrimeraPagina = driver.findElement(By.id("firstHeading"));//Recuperamos el elemento que tiene el titulo en la p�gina abierta
-		
-			String titulo=tituloPrimeraPagina.getText();//Obtenemos el titulo de la pagina abierta
+		ofertas.click();
 
-			assertEquals(tituloSeleniumEsperado,titulo);//Comprobamos el titulo obtenido con el esperado
-			
-			
-	}//Fin realizarBusquedawiki
-	
+		// Cada producto está en un contendor con que tiene la clase
+		// "c-product-card__wrapper"
+		List<WebElement> productos = driver.findElements(By.className("c-product-card__wrapper"));
+
+		boolean oferta = false;
+		for (WebElement we : productos) {
+			oferta = false;
+			// System.out.println(we1.getAttribute("data-name"));
+			List<WebElement> we2 = we.findElements(By.cssSelector("div"));
+			for (WebElement w : we2) {
+				// System.out.println(w.getAttribute("class"));
+				// Los productos con descuento tiene la clase "c-product-card__discount
+				// cy-product-discount"
+				if (w.getAttribute("class").equals("c-product-card__discount cy-product-discount")) {
+					oferta = true;
+				}
+			}
+			if(!oferta){
+				break;
+			}
+		}
+
+		assertTrue(oferta);
+
+	}// Fin comprobarOfertas
+
 	@Test
-	public void test_WikiRandomAndBack() {
-		/*******
-		 * Vamos abrir una página de la wikipedia, pulsaremos en el enlace Pagina Aleatoria y a continuaci�n,
-		 * pulsaremos sobre Go Back del navegador
-		 *****/
-		
-			 String tituloHomeEsperado="Wikipedia, la enciclopedia libre";
-			driver.get("https://www.wikipedia.es"); //Abrimos la p�gina de la wikipedia 						
-			WebElement randomPage = driver.findElement(By.linkText("Página aleatoria"));
-			randomPage.click();//Ejecutamos la accion de click para que pinche en el enlace
-			
-			//driver.navigate().back();//Ejecutamos el Atrás del navegador para cargar de nuevo la p�gina inicial
-			
-			String titulo=driver.getTitle();//Obtenemos el titulo de la pagina abierta
-			
-			assertEquals(tituloHomeEsperado,titulo);//Comprobamos el titulo obtenido con el esperado
-			
-			
-	}//Fin test_WikiRandomAndBack
+	public void test_cantidadNegativaBoton() {
+		boolean positivo;
+
+		driver.get("https://www.pccomponentes.com"); // Abrimos la p�gina de pccomponentes
+
+		List<WebElement> productos = driver.findElements(By.id("producto-interesa")); // Lista de productos en pagina
+																						// principal
+
+		productos.get(0).click(); // Seleccionamos el primer producto enocntrado
+
+		WebElement botonMenos = driver
+				.findElement(By.cssSelector("button[class='btn btn-secondary quantity-modify quantity-sub']"));
+
+		int i = 5;
+		while (i > 0) {
+			int click = 1;
+			System.out.println("click: " + 1);
+			botonMenos.click();
+			i--;
+			click++;
+		}
+
+		WebElement cantidad = driver.findElement(By.id("article-quantity"));
+
+		positivo = Integer.parseInt(cantidad.getAttribute("value")) > 0;
+		System.out.println(positivo);
+
+		assertTrue(positivo);
+
+	}// Fin test_cantidadNegativaBoton
+
+	@Test
+	public void test_cantidadNegativaInput() {
+		boolean positivo;
+
+		driver.get("https://www.pccomponentes.com"); // Abrimos la p�gina de pccomponentes
+
+		List<WebElement> productos = driver.findElements(By.id("producto-interesa")); // Lista de productos en pagina
+																						// principal
+
+		productos.get(0).click(); // Seleccionamos el primer producto enocntrado
+
+		WebElement cantidad = driver.findElement(By.id("article-quantity"));
+
+		cantidad.clear();// eliminamos en valor por defecto
+		cantidad.sendKeys("-5");// añadimos cantidad negativa
+
+		System.out.println(cantidad.getAttribute("value"));
+		positivo = Integer.parseInt(cantidad.getAttribute("value")) > 0;
+		System.out.println(positivo);
+
+		assertTrue(positivo);
+
+	}// Fin test_cantidadNegativaInput
+
+	@Test
+	public void test_ordenDescendente() {
+		boolean positivo = true;
+
+		driver.get("https://www.pccomponentes.com"); // Abrimos la p�gina de pccomponentes
+
+		WebElement menu = driver.findElement(By.cssSelector("h3[class='c-main-menu__name']"));
+
+		menu.click();
+
+		WebElement smartphonesT = driver.findElement(By.cssSelector("a[title='Smartphones y Telefonía']"));
+
+		Actions actions = new Actions(driver);
+
+		// colocamos cursor sobre Smartphones y telefonía para visualizar más opciones
+		// de menú
+		actions.moveToElement(smartphonesT).perform();
+
+		WebElement smartphones = driver.findElement(By.cssSelector("a[title='Smartphones']"));
+
+		smartphones.click();
+
+		Select combo = new Select(driver.findElement(By.id("listOrder")));
+		combo.selectByValue("price-desc");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        //Comprobamos precios de productos
+
+		double price = 1000000000;
+        List<WebElement> productos = driver.findElements(By.className("col-xs-6"));
+
+		for(WebElement we: productos){
+
+            WebElement elementoPrecio= we.findElement(By.cssSelector("article"));
+            System.out.println(elementoPrecio.getAttribute("data-name"));
+			double actualPrice = Double.parseDouble(elementoPrecio.getAttribute("data-price"));
+			System.out.println("actual price: " + actualPrice);
+
+			if(price >= actualPrice){
+				price = actualPrice;
+			}else{
+				positivo = false;
+				break;
+			}
+		}
+
+		assertTrue(positivo);
+	}//Fin test_ordenDescendente
+
+	@Test
+	public void test_ordenAscendente() {
+		boolean positivo = true;
+
+		driver.get("https://www.pccomponentes.com"); // Abrimos la p�gina de pccomponentes
+
+		WebElement menu = driver.findElement(By.cssSelector("h3[class='c-main-menu__name']"));
+
+		menu.click();
+
+		WebElement smartphonesT = driver.findElement(By.cssSelector("a[title='Smartphones y Telefonía']"));
+
+		Actions actions = new Actions(driver);
+
+		// colocamos cursor sobre Smartphones y telefonía para visualizar más opciones
+		// de menú
+		actions.moveToElement(smartphonesT).perform();
+
+		WebElement smartphones = driver.findElement(By.cssSelector("a[title='Smartphones']"));
+
+		smartphones.click();
+
+		Select combo = new Select(driver.findElement(By.id("listOrder")));
+		combo.selectByValue("price-asc");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        //Comprobamos precios de productos
+
+		double price = 0;
+        List<WebElement> productos = driver.findElements(By.className("col-xs-6"));
+
+		for(WebElement we: productos){
+
+            WebElement elementoPrecio= we.findElement(By.cssSelector("article"));
+            System.out.println(elementoPrecio.getAttribute("data-name"));
+			double actualPrice = Double.parseDouble(elementoPrecio.getAttribute("data-price"));
+			System.out.println("actual price: " + actualPrice);
+
+			if(price <= actualPrice){
+				price = actualPrice;
+			}else{
+				positivo = false;
+				break;
+			}
+		}
+
+		assertTrue(positivo);
+	}//Fin test_ordenAscendente
 	
 	@After
 	public void shutdown() {
